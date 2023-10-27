6b5ee14145bbceeff37e1e4bd28336a95a99dde3
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-27 16:06:00 -0700

    ok

---
+++ {{ cookiecutter.project_slug }}/cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"os"
+
+	"github.com/taylormonacelli/goldbug"
+	"github.com/taylormonacelli/{{ cookiecutter.project_slug }}"
+)
+
+var (
+	verbose   bool
+	logFormat string
+)
+
+func main() {
+	flag.BoolVar(&verbose, "verbose", false, "Enable verbose output")
+	flag.BoolVar(&verbose, "v", false, "Enable verbose output (shorthand)")
+
+	flag.StringVar(&logFormat, "log-format", "", "Log format (text or json)")
+
+	flag.Parse()
+
+	if verbose || logFormat != "" {
+		if logFormat == "json" {
+			goldbug.SetDefaultLoggerJson(slog.LevelDebug)
+		} else {
+			goldbug.SetDefaultLoggerText(slog.LevelDebug)
+		}
+	}
+
+	code := {{ cookiecutter.project_slug }}.Main()
+	os.Exit(code)
+}
